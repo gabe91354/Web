718e42f48b127d1940bebd3368a35f4ff74e226e
Gabe Alexis <167690779+gabe91354@users.noreply.github.com>
2024-10-07 15:30:02 +0800

    dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -1,12 +1,20 @@
-<?php 
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "asence";
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "asence";
-$dsn = "mysql:host={$host};dbname={$dbname}";
+    // Setup PDO DSN (Data Source Name)
+    $dsn = "mysql:host={$db_server};dbname={$db_name}";
 
-$pdo = new PDO($dsn, $user, $password);
-$pdo->exec("SET time_zone = '+08:00';");
-
+    // Try to establish a PDO connection
+    try {
+        $pdo = new PDO($dsn, $db_user, $db_pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->exec("SET time_zone = '+08:00';");
+        echo "You are connected!";
+    } catch (PDOException $e) {
+        // Handle connection error
+        echo "Could not connect: " . $e->getMessage();
+    }
 ?>
